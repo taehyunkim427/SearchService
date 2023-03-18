927a9aced8e434e6c5048d6ca3a0747cc7ff7084
taehyunkim427 <thkim427@naver.com>
2023-03-18 15:56:11 +0900

    [create project] connect h2 database

--- src/main/java/search/SearchServiceApplication.java
+++ src/main/java/search/SearchServiceApplication.java
@@ -1,4 +1,4 @@
-package search.blog;
+package search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/search/SearchServiceApplicationTests.java
+++ src/test/java/search/SearchServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package search.blog;
+package search;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
