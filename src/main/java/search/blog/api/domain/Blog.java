cc796e9af3c258425a889b9018f2398d68e45cbb
taehyunkim427 <thkim427@naver.com>
2023-03-21 07:18:41 +0900

    [설정 변경] 블로그 도메인 OffsetDateTime 수정, H2 인메모리 설정

--- src/main/java/search/blog/api/domain/Blog.java
+++ src/main/java/search/blog/api/domain/Blog.java
@@ -1,14 +1,19 @@
 package search.blog.api.domain;
 
 import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
 
 @Getter
+@Setter
 public class Blog {
     private String blogname;
 
     private String contents;
 
-    private String datetime;
+    //[YYYY]-[MM]-[DD]T[hh]:[mm]:[ss].000+[tz]
+    private OffsetDateTime datetime;
 
     private String thumbnail;
 
