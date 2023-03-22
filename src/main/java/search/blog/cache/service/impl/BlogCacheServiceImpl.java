0b27eb4b18b4fbc0822556a6804e635298d58106
taehyunkim427 <thkim427@naver.com>
2023-03-22 14:39:00 +0900

    [Spring data JPA] JPQL 리스트 결과 수정

--- src/main/java/search/blog/cache/service/impl/BlogCacheServiceImpl.java
+++ src/main/java/search/blog/cache/service/impl/BlogCacheServiceImpl.java
@@ -23,19 +23,19 @@ public class BlogCacheServiceImpl implements BlogCacheService {
      * 1. "Search" 캐시에서 "Top" 키를 사용하여 저장된 결과를 찾습니다.
      * 2. 저장된 결과가 존재하면, 그 결과를 반환합니다.
      * 3. 저장된 결과가 없으면, 메서드가 실행되고 결과가 캐시에 "Top" 키로 저장되고 반환됩니다.
-     * 캐시된 결과는 작성된 후 30초 동안만 유효 (apllication.yml에서 수정)
-     * spring.cache.caffeine.spec: expireAfterWrite=30s
+     * 캐시된 결과는 작성된 후 10초 동안만 유효 (apllication.yml에서 수정)
+     * spring.cache.caffeine.spec: expireAfterWrite=10s
      *
-     * web server에서 nuxtServerInit Lyfecycle 훅의 스케쥴링을 통해 10초마다 요청을 보내고 있습니다.
+     * web 에서 랭킹 nav를 클릭 시 캐시 요청이 발생합니다
      */
     @Cacheable(value = "Search", key = "Top")
     public List<TopSearchQuery> getTop10QueriesWithCountFromCache() {
         List<Object[]> topQueriesWithCount = searchRepository.getTop10QueriesWithCount();
         List<TopSearchQuery> topSearchQueries = topQueriesWithCount.stream()
+                .limit(10)
                 .map(result -> new TopSearchQuery((String) result[0], (Long) result[1]))
                 .collect(Collectors.toList());
         return topSearchQueries;
-
     }
 
 }
