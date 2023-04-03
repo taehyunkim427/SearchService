e4db24feca2a4fe4a055a552e5f076e396154768
taehyunkim427 <thkim427@naver.com>
2023-04-04 03:29:19 +0900

    [도커] 도커파일 설정

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# 베이스 이미지로 amazon corretto OpenJDK 17을 사용
+FROM amazoncorretto:17 as build
+
+# 작업 디렉토리를 설정
+WORKDIR /app
+
+# 프로젝트의 모든 파일을 작업 디렉토리에 복사
+COPY . .
+
+# Gradle을 사용하여 프로젝트를 빌드
+RUN ./gradlew clean build
+
+# 빌드한 파일을 실행할 새 이미지를 생성
+FROM amazoncorretto:17
+
+# 작업 디렉토리를 설정
+WORKDIR /app
+
+# 빌드 이미지에서 생성된 JAR 파일을 현재 이미지로 복사
+COPY --from=build /app/build/libs/*.jar /app/app.jar
+
+# 컨테이너가 시작되면 실행할 명령을 설정
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
+
+# 컨테이너가 수신할 포트를 지정
+EXPOSE 8080
--- src/main/java/search/config/WebConfig.java
+++ src/main/java/search/config/WebConfig.java
@@ -1,6 +1,5 @@
 package search.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,13 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${web.server}")
-    private String webServer;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(webServer)
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*");
     }
